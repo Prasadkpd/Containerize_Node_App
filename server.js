d6326d25658366eff452c2c6e2e569958a1f9c34
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-08-14 07:35:52 +0530

    node app done

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/',(req, res) => res.send("Hello World"))
+
+app.listen(port,()=> console.log(`App Listening at http://localhost:${port}`))
